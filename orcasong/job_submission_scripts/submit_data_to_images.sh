d8afb52076e29c760e735524f693db9ab89934d7
ViaFerrata <michimoser@onlinehome.de>
2018-12-07 15:49:49 +0100

    Add random_noise config.

--- orcasong/job_submission_scripts/submit_data_to_images.sh
+++ orcasong/job_submission_scripts/submit_data_to_images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#PBS -l nodes=1:ppn=4:sl32g,walltime=1:30:00
+#PBS -l nodes=1:ppn=4:sl32g,walltime=06:00:00
 #PBS -o /home/woody/capn/mppi033h/logs/submit_data_to_images_${PBS_JOBID}_${PBS_ARRAYID}.out -e /home/woody/capn/mppi033h/logs/submit_data_to_images_${PBS_JOBID}_${PBS_ARRAYID}.err
 # first non-empty non-comment line ends PBS options
 
@@ -12,7 +12,8 @@
 #            elec-CC = 1200 files, number of jobs needed = 20,
 #            elec-NC = 1200 files, number of jobs needed = 20,
 #            tau-CC = 1800 files (half the size of other interaction channels), number of jobs needed = 30 (half walltime)
-# mupage: 20000 files, TODO
+# mupage: 20000 files, with files_per_job=1000 20 jobs needed.
+# random_noise: 500 files, with files_per_job=
 
 
 #--- USER INPUT ---##
@@ -22,12 +23,17 @@ python_env_folder=/home/hpc/capn/mppi033h/.virtualenv/python_3_env/
 code_folder=/home/woody/capn/mppi033h/Code/OrcaSong/orcasong
 
 detx_filepath=${code_folder}/detx_files/orca_115strings_av23min20mhorizontal_18OMs_alt9mvertical_v1.detx
-config_file=${code_folder}/config/orca_115l_mupage_rn_neutr_classifier/conf_ORCA_115l_1-5GeV_xyz-t.toml
+config_file=${code_folder}/config/orca_115l_mupage_rn_neutr_classifier/conf_ORCA_115l_random_noise_xyz-c.toml
 
-particle_type=elec-NC
-mc_prod=neutr_1-5GeV
+particle_type=random_noise
+mc_prod=random_noise
 
-files_per_job=60 # total number of files per job, e.g. 10 jobs for 600: 600/10 = 60
+# total number of files per job, e.g. 10 jobs for 600: 600/10 = 60. For neutrin
+# For neutrinos and random_noise n=60 with PBS -l nodes=1:ppn=4:sl32g,walltime=3:00:00
+# For mupage: n=1000 with PBS -l nodes=1:ppn=4:sl32g,walltime=15:00:00
+files_per_job=50
+
+n_cores=2 # number of available CPU cores, be careful about memory as well! Currently only working for 4 cores!
 
 #--- USER INPUT ---##
 
@@ -66,21 +72,34 @@ folder="${folder_ip_files_arr[${mc_prod}]}"
 
 # run
 
-no_of_loops=$((${files_per_job}/4)) # divide by 4 cores -> e.g, 15 4-core loops needed for files_per_job=60
+no_of_loops=$((${files_per_job}/${n_cores})) # divide by 4 cores -> e.g, 15 4-core loops needed for files_per_job=60
 file_no_start=$((1+((${n}-1) * ${files_per_job}))) # filenumber of the first file that is being processed by this script (depends on JobArray variable 'n')
+
+# currently only working for 4 cores
+
+#for (( k=1; k<=${no_of_loops}; k++ ))
+#do
+#    file_no_loop_start=$((${file_no_start}+(k-1)*${n_cores}))
+#    thread1=${file_no_loop_start}
+#    thread2=$((${file_no_loop_start} + 1))
+#    thread3=$((${file_no_loop_start} + 2))
+#    thread4=$((${file_no_loop_start} + 3))
+#
+#    (time taskset -c 0  python ${code_folder}/data_to_images.py -c ${config_file} ${folder}/${filename}.${thread1}.h5 ${detx_filepath} > ./job_logs/cout/${filename}.${thread1}.txt) &
+#    (time taskset -c 1  python ${code_folder}/data_to_images.py -c ${config_file} ${folder}/${filename}.${thread2}.h5 ${detx_filepath} > ./job_logs/cout/${filename}.${thread2}.txt) &
+#    (time taskset -c 2  python ${code_folder}/data_to_images.py -c ${config_file} ${folder}/${filename}.${thread3}.h5 ${detx_filepath} > ./job_logs/cout/${filename}.${thread3}.txt) &
+#    (time taskset -c 3  python ${code_folder}/data_to_images.py -c ${config_file} ${folder}/${filename}.${thread4}.h5 ${detx_filepath} > ./job_logs/cout/${filename}.${thread4}.txt) &
+#    wait
+#done
 
 for (( k=1; k<=${no_of_loops}; k++ ))
 do
-    file_no_loop_start=$((${file_no_start}+(k-1)*4))
+    file_no_loop_start=$((${file_no_start}+(k-1)*${n_cores}))
     thread1=${file_no_loop_start}
     thread2=$((${file_no_loop_start} + 1))
-    thread3=$((${file_no_loop_start} + 2))
-    thread4=$((${file_no_loop_start} + 3))
 
     (time taskset -c 0  python ${code_folder}/data_to_images.py -c ${config_file} ${folder}/${filename}.${thread1}.h5 ${detx_filepath} > ./job_logs/cout/${filename}.${thread1}.txt) &
     (time taskset -c 1  python ${code_folder}/data_to_images.py -c ${config_file} ${folder}/${filename}.${thread2}.h5 ${detx_filepath} > ./job_logs/cout/${filename}.${thread2}.txt) &
-    (time taskset -c 2  python ${code_folder}/data_to_images.py -c ${config_file} ${folder}/${filename}.${thread3}.h5 ${detx_filepath} > ./job_logs/cout/${filename}.${thread3}.txt) &
-    (time taskset -c 3  python ${code_folder}/data_to_images.py -c ${config_file} ${folder}/${filename}.${thread4}.h5 ${detx_filepath} > ./job_logs/cout/${filename}.${thread4}.txt) &
     wait
 done
 
