89a9c9c2427eb6a0213d1eb1c72fcd121f821e13
ViaFerrata <michimoser@onlinehome.de>
2019-02-05 11:19:10 +0100

    Minor docs changes.

--- docs/conf.py
+++ docs/conf.py
@@ -117,6 +117,11 @@ html_logo = "_static/orcasong_wide_transparent_white.png"
 #
 # html_theme_options = {}
 
+# The name for this set of Sphinx documents.  If None, it defaults to
+# "<project> v<release> documentation".
+html_title = "OrcaSong {}".format(orcasong.__version__)
+
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
