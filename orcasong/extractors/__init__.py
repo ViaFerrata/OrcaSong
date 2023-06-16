200b127d10f91288c94f83ab6d61a7e694e0a706
Rodrigo Gracia Ruiz <mppi104h@woodycap5.nhr.fau.de>
2023-06-16 14:32:27 +0200

    add test extractor

--- orcasong/extractors/__init__.py
+++ orcasong/extractors/__init__.py
@@ -1,6 +1,7 @@
 from .neutrino_chain import (
     get_neutrino_mc_info_extr,
     get_neutrino_mc_info_extr_glashow,
+    get_neutrino_mc_info_extr_glashow_test,
     get_muon_mc_info_extr,
     get_real_data_info_extr,
     get_random_noise_mc_info_extr,
